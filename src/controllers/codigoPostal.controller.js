7fa3e47b332939680dbcb9113cdf3c6837677a38
fakeblackspectre <fakeblackspectre@protonmail.com>
2022-05-19 16:16:36 +0100

    alterações: alteração no método de obter um código postal

--- src/controllers/codigoPostal.controller.js
+++ src/controllers/codigoPostal.controller.js
@@ -5,14 +5,17 @@ const catchAsync = require('../utils/catchAsync');
 const { codigoPostalService } = require('../services');
 
 const getCodigosPostais = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['cod_distrito', 'cod_concelho']);
+  const filter = pick(req.query, ['cod_distrito', 'cod_concelho', 'num_cod_postal', 'ext_cod_postal']);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
   const result = await codigoPostalService.queryCodigosPostais(filter, options);
   res.send(result);
 });
 
 const getCodigoPostal = catchAsync(async (req, res) => {
-  const codigoPostal = await codigoPostalService.getCodigoPostalByCodigos(req.query.codigoP1, req.query.codigoP2);
+  const codigoPostal = await codigoPostalService.getCodigoPostalByCodigos(
+    req.query.num_cod_postal,
+    req.query.ext_cod_postal
+  );
   if (!codigoPostal) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Código Postal not found');
   }
